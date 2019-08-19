26b69ec4f7b742a99d7f92b7eb37076aef489362
Carlos Eduardo B <37811174+centuri123@users.noreply.github.com>
2019-08-19 13:23:46 -0300

    Update exercicio6.php

--- exercicio6.php
+++ exercicio6.php
@@ -1,34 +1,46 @@
 <?php
   
   $peso = readline("Digite seu Peso: ");
-  $altura = readline("Digite sua Altura: ");
+  if($peso > 0)
+  {
+      $altura = readline("Digite sua Altura: ");
+      if($altura > 0)
+      {
+          $imc = ($peso/($altura*$altura));
 
-  $imc = ($peso/($altura*$altura));
-
-  if($imc < 18.5)
-  {
-    echo "Seu IMC eh: " . number_format($imc,2) . "\n";
-    echo "Cuidado, você esta muito abaixo do peso\n";
+          if($imc < 18.5)
+          {
+            echo "Seu IMC eh: " . number_format($imc,2) . "\n";
+            echo "Cuidado, você esta muito abaixo do peso\n";
+          }
+          else if($imc <= 24.9)
+          {
+            echo "Seu IMC eh: " . number_format($imc,2) . "\n";
+            echo "Seu peso esta normal\n";
+          } 
+          else if($imc <= 29.9)
+          {
+            echo "Seu IMC eh: " . number_format($imc,2) . "\n";
+            echo "Voce esta sobrepeso\n";
+          }
+          else if($imc <= 39.9)
+          {
+            echo "Seu IMC eh: " . number_format($imc,2) . "\n";
+            echo "Cuidado, voce esta com um grau de obesidade\n";
+          }
+          else
+          {
+            echo "Seu IMC eh: " . number_format($imc,2) . "\n";
+            echo "Procure um medico especialista, voce esta com um grau de obesidade grave!!";
+          }
+      }
+      else
+      {
+          echo "Valor Invalido!!";
+      }
   }
-  else if($imc <= 24.9)
+  else
   {
-    echo "Seu IMC eh: " . number_format($imc,2) . "\n";
-    echo "Seu peso esta normal\n";
-  } 
-  else if($imc <= 29.9)
-  {
-    echo "Seu IMC eh: " . number_format($imc,2) . "\n";
-    echo "Voce esta sobrepeso\n";
+      echo "Valor Invalido!!"; 
   }
-  else if($imc <= 39.9)
-  {
-    echo "Seu IMC eh: " . number_format($imc,2) . "\n";
-    echo "Cuidado, voce esta com um grau de obesidade\n";
-  }
-  else 
-  {
-    echo "Seu IMC eh: " . number_format($imc,2) . "\n";
-    echo "Procure um medico especialista, voce esta com um grau de obesidade grave!!";
-  }
-
 ?>
